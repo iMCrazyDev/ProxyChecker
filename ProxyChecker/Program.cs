3b845be9c2cbb4578525df25d0335514ea3ad957
iMCrazyDev <iMCrazy.siXTouch@yandex.ru>
2021-09-28 04:22:26 +0300

    Initial commit

---
+++ ProxyChecker/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ProxyChecker
+{
+    internal class Program
+    {
+        const string proxyFileName = "proxy.txt";
+        const int maxThreadsNumber = 100;
+        const Leaf.xNet.ProxyType proxyType = Leaf.xNet.ProxyType.HTTP;
+        const int IterationsAmount = 10;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Файл с прокси {proxyFileName}, тип {proxyType}");
+
+            ProxyList proxyList = new ProxyList();
+            int loadedProxyNumber = proxyList.ReadFromFile(proxyFileName, proxyType);
+
+            Console.WriteLine($"Загружено {loadedProxyNumber} проксей"); 
+
+            if(loadedProxyNumber == 0)
+            {
+                Console.ReadKey(); 
+                return;
+            }
+
+            int threadsAmount = Math.Min(maxThreadsNumber, loadedProxyNumber);
+
+            Console.WriteLine($"Запускаем {threadsAmount} потоков");
+
+            var listThreads = StartThreads(threadsAmount, proxyList);
+
+            while (listThreads.Exists(x => x.IsAlive))
+            {
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Итерации завершены");
+            Console.ReadKey();
+        }
+
+        static List<Thread> StartThreads(int amount, ProxyList proxyList)
+        {
+            var list = new List<Thread>();
+            for (int i = 0; i < amount; i++)
+            {
+                int index = i;
+                var thread = new Thread(() => { ThreadBody(index, proxyList.GetProxyClient()); });
+                list.Add(thread);
+                thread.Start();
+            }
+
+            return list;
+        }
+
+        static string RequestUrl = "http://icanhazip.com/";
+
+        static void ThreadBody(int index, Leaf.xNet.ProxyClient proxyClient)
+        {
+            Leaf.xNet.HttpRequest httpRequest = new Leaf.xNet.HttpRequest()
+            {
+                Proxy = proxyClient,
+                ConnectTimeout = 10000
+            };
+
+            string tempText = $"Поток {index}, прокси {proxyClient.Host}:{proxyClient.Port}";
+            
+            for (int i = 0; i < IterationsAmount; i++)
+            {
+                try
+                {
+                    var page = httpRequest.Get(RequestUrl).ToString();
+                    Console.WriteLine($"{tempText} Ответ сайта: {page}");
+
+                }
+                catch (Exception e) { Console.WriteLine(tempText + " Error " + e.Message); }
+            }
+        }
+    }
+}
---
+++ ProxyChecker/ProxyList.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ProxyChecker
+{
+    internal class ProxyList
+    {
+        private List<Leaf.xNet.ProxyClient> proxyClients;
+        private object lockerProxy;
+        private int indexOfCurrentProxyFromPool;
+
+        public ProxyList()
+        {
+            proxyClients = new List<Leaf.xNet.ProxyClient>();
+            lockerProxy = new object();
+            indexOfCurrentProxyFromPool = 0;
+        }
+
+        public int ReadFromFile(string fileName, Leaf.xNet.ProxyType proxyType)
+        {
+            try
+            {
+                var fileLines = File.ReadAllLines(fileName).ToList();
+
+                fileLines.ForEach(line =>
+                {
+                    Leaf.xNet.ProxyClient proxyClient;
+                    if (Leaf.xNet.ProxyClient.TryParse(proxyType, line, out proxyClient)) 
+                    {
+                        proxyClients.Add(proxyClient);
+                    }
+                });
+
+                return fileLines.Count;
+            }
+            catch { }
+
+            return 0;
+        }
+
+        public Leaf.xNet.ProxyClient GetProxyClient()
+        {
+            lock (lockerProxy)
+            {
+                if (indexOfCurrentProxyFromPool == proxyClients.Count)
+                {
+                    indexOfCurrentProxyFromPool = 0;
+                }
+
+                return proxyClients[indexOfCurrentProxyFromPool++];
+            }
+        }
+    }
+}
